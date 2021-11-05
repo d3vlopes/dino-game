82f67cd6ae99ae8b5240111dbd2a1e8084ba3bba
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-11-05 19:44:44 -0300

    add cactus

--- script.js
+++ script.js
@@ -1,37 +1,56 @@
-const dino = document.querySelector(".dino");
+const dino = document.querySelector('.dino')
+const background = document.querySelector('.background')
+
+let isJumping = false
 
 function handleKeyUp(event) {
   // console.log('Pressionou uma tecla')
 
-  if (event.keyCode === 32) {
+  if (event.keyCode === 32 || event.keyCode === 38) {
     // keycode.info
-    jump();
+    if (!isJumping) {
+      jump()
+    }
   }
 }
 
 function jump() {
-  let position = 0;
+  let position = 0
+
+  isJumping = true
 
   let upInterval = setInterval(() => {
     if (position >= 150) {
       // para de subir
-      clearInterval(upInterval);
+      clearInterval(upInterval)
 
       let downInterval = setInterval(() => {
         if (position <= 0) {
-          clearInterval(downInterval);
+          clearInterval(downInterval)
+          isJumping = false
         } else {
-          position = position - 20;
+          position = position - 20
 
-          dino.style.bottom = `${position}px`;
+          dino.style.bottom = `${position}px`
         }
-      }, 20);
+      }, 20)
     } else {
-      position = position + 20;
+      position = position + 20
 
-      dino.style.bottom = `${position}px`;
+      dino.style.bottom = `${position}px`
     }
-  }, 20);
+  }, 20)
 }
 
-document.addEventListener("keyup", handleKeyUp);
+function createCactus() {
+  const cactus = document.createElement('div')
+  let cactusPosition = 1000
+
+  cactus.classList.add('cactus')
+  cactus.style.left = `${cactusPosition}px`
+  // adiciona a div cactus dentro da div background
+  background.appendChild(cactus)
+}
+
+createCactus()
+document.addEventListener('keyup', handleKeyUp)
